d4ad6b57c7599355e1ec36b386b6e8cf1920012c
PacersCyc <452720855@qq.com>
2019-12-04 19:32:13 +0800

    自动更新

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const path = require('path')
 const uuidv4 = require('uuid/v4')
 const { app, BrowserWindow, Menu, ipcMain, dialog } = require('electron')
 const isDev = require('electron-is-dev')
+const { autoUpdater } = require('electron-updater')
 const Store = require('electron-store')
 const settingStore = new Store({
   name: 'Settings'
@@ -30,6 +31,54 @@ app.on('ready', () => {
   //     nodeIntegration: true
   //   }
   // })
+  if (isDev) {
+    autoUpdater.updateConfigPath = path.join(__dirname, 'dev-app-update.yml')
+  }
+  autoUpdater.autoDownload = false
+  autoUpdater.checkForUpdates()
+  autoUpdater.on('error', (error) => {
+    dialog.showErrorBox('Error ', error === null ? "ubknown" : error.stack)
+  })
+  autoUpdater.on('checking-for-update', () => {
+    console.log('Checking for update...')
+  })
+  autoUpdater.on('update-available', () => {
+    dialog.showMessageBox({
+      type: 'info',
+      title: '应用有新的版本',
+      message: '发现新版本，是否现在更新',
+      buttons: ['是','否']
+    }, (buttonIndex) => {
+      if (buttonIndex === 0) {
+        autoUpdater.downloadUpdate()
+      }
+    })
+  })
+  autoUpdater.on('update-not-available', () => {
+    dialog.showMessageBox({
+      title: '没有新版本',
+      message: '当前已经是最新版本'
+    })
+  })
+
+  autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = 'Download speed: ' + progressObj.bytesPerSecond
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%'
+    log_message = log_message + ' (' + progressObj.transferred + '/' + progressObj.total + ')'
+    console.log(log_message)
+  })
+
+  autoUpdater.on('update-downloaded', () => {
+    dialog.showMessageBox({
+      title: '安装更新',
+      message: '更新下载完毕，应用将重启并进行安装'
+    }, () => {
+      setImmediate(() => {
+        autoUpdater.quitAndInstall()
+      })
+    })
+  })
+
   const mainWindowConfig = {
     width: 1024,
     height: 680,
